cd99cc4f59e28d2254cff7d77be35fbdf95448c2
SunPodder <73415680+SunPodder@users.noreply.github.com>
2023-10-27 13:06:41 +0600

    add module: window

---
+++ lua/tldr/window.lua
@@ -0,0 +1,40 @@
+local config = require("tldr.config")
+
+local M = {}
+
+-- Create a new window
+-- @param opts table
+-- @return winid number|nil
+function M.new(opts)
+	local c = config.get("window")
+
+	if opts then
+		vim.tbl_deep_extend("force", c, opts)
+	end
+
+	local bufnr = vim.api.nvim_create_buf(false, true)
+    local winid = vim.api.nvim_open_win(bufnr, true, c)
+
+	if winid == 0 then
+		vim.notify("TLDR: Failed to open window", vim.log.levels.ERROR)
+		return nil
+	end
+
+    return winid
+end
+
+-- Write lines to a window
+-- @param winid number
+-- @param lines table
+-- @return nil
+function M.write(winid, lines)
+	local bufnr = vim.api.nvim_win_get_buf(winid)
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+end
+
+function M.close(winid)
+	vim.api.nvim_win_close(winid, true)
+end
+
+return M
+
